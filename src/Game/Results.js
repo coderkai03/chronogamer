1fcf1ab7e136c8deb05cf634f57b92ef74a4ad33
Riankai1073 <riancorci@gmail.com>
2023-08-03 20:56:11 -0700

    displays total rounds and pts

---
+++ src/Game/Results.js
@@ -0,0 +1,21 @@
+import { useLocation } from "react-router-dom/cjs/react-router-dom.min";
+
+const Results = ({rounds, points}) => {
+    const location = useLocation()
+
+    const finalRounds = location.state.rounds
+    const finalPoints = location.state.points
+
+    console.log('RESULTS ROUNDS: ', finalRounds)
+    console.log('RESULTS POINTS: ', finalPoints)
+
+    return (
+        <div>
+            <h1>Results</h1>
+            <p>Rounds: {finalRounds}</p>
+            <p>Points: {finalPoints}</p>
+        </div>
+    );
+}
+ 
+export default Results;
