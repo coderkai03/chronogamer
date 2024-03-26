981fea6298c10f94fea4a8725e3799df8d071a1a
Rian Corcino <riancorci@gmail.com>
2024-03-25 19:37:36 -0700

    created login/signup screens

--- src/Game/Home.js
+++ src/Game/Home.js
@@ -6,24 +6,50 @@ import { useHistory } from "react-router-dom/cjs/react-router-dom.min";
 const Home = () => {
     const history = useHistory()
 
-    const handleSubmit = (e) => {
+    const [isLogin, setIsLogin] = useState(true)
+
+    const handleLogin = (e) => {
         e.preventDefault();
         history.push('/round-select');
-      }
+    }
+
+    const handleSignup = (e) => {
+        e.preventDefault();
+        history.push('/round-select');
+    }
+
+    const switchLogin = () => {
+        setIsLogin(!isLogin)
+    }
 
     return (
         <div className="home">
             <div className="content-div">
-                <div>
+                {isLogin && <div>
                     <h3>LOG IN</h3>
                     <form>
                         <label for="uname">Username</label>
                         <input type="text" id="uname" name="uname"/>
                         <label for="password">Password</label>
                         <input type="text" id="password" name="password"/>
-                        <button onClick={handleSubmit}>LOG IN</button>
+                        <button onClick={handleLogin}>LOG IN</button>
                     </form>
-                </div>
+
+                    <p className="accountSwitch" onClick={switchLogin}><u>SIGN UP</u></p>
+                </div>}
+
+                {!isLogin && <div>
+                    <h3>SIGN UP</h3>
+                    <form>
+                        <label for="uname">Username</label>
+                        <input type="text" id="uname" name="uname"/>
+                        <label for="password">Password</label>
+                        <input type="text" id="password" name="password"/>
+                        <button onClick={handleSignup}>SIGN UP</button>
+
+                        <p className="accountSwitch" onClick={switchLogin}><u>LOG IN</u></p>
+                    </form>
+                </div>}
             </div>
         </div>
     );
